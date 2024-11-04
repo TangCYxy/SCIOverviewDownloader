ee15caee0d4561769e1453f6c981db89a7ad7b3e
tc <tc9926271333@gmail.com>
2024-11-04 21:27:45 +0800

    bug fix: color identification

--- utils.py
+++ utils.py
@@ -5,18 +5,23 @@ import math
 
 max_color_distance = math.sqrt(3 * (255 ** 2))
 
-def findColor(x1, y1, x2, y2, target_color, confidence):
+def findColor(x1, y1, x2, y2, targetColorStr, confidence):
     """
     避免框选了太大的图像，导致遍历像素点太麻烦（后期可以改并发）
     :param x1:
     :param y1:
     :param x2:
     :param y2:
-    :param target_color:
+    :param targetColorStr:
     :param confidence: 0-1
     :return:
     """
     img = pyautogui.screenshot(region=(x1, y1, x2, y2))
+    img.show()
+    targetColor = []
+    targetColor.append(int(targetColorStr[4:6], 16))
+    targetColor.append(int(targetColorStr[2:4], 16))
+    targetColor.append(int(targetColorStr[0:2], 16))
     pixels = img.load()
     width, height = img.size
 
@@ -24,7 +29,7 @@ def findColor(x1, y1, x2, y2, target_color, confidence):
         for y in range(height):
             r, g, b = pixels[x, y]
             # 判断相似度，计算欧式距离
-            distance = math.sqrt((target_color[0] - r) ** 2 + (target_color[1] - g) ** 2 + (target_color[2] - b) ** 2)
+            distance = math.sqrt((targetColor[0] - r) ** 2 + (targetColor[1] - g) ** 2 + (targetColor[2] - b) ** 2)
             simularity = (1 - distance / max_color_distance)
             if simularity > confidence:
                 return x, y
